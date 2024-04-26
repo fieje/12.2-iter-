9ab07f9926aab419e13be69373566335b08c2feb
jack <144657795+fieje@users.noreply.github.com>
2024-04-26 20:56:55 +0300

    Add files via upload

---
+++ 12.2(iter)/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+public class ListNode
+{
+    public int Value { get; set; }
+    public ListNode Next { get; set; }
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        Value = val;
+        Next = next;
+    }
+}
+public class Solution
+{
+    public static void SplitList(ListNode head, out ListNode positiveList, out ListNode negativeList)
+    {
+        positiveList = null;
+        negativeList = null;
+
+        ListNode currentPos = null;
+        ListNode currentNeg = null;
+
+        while (head != null)
+        {
+            if (head.Value >= 0)
+            {
+                if (positiveList == null)
+                {
+                    positiveList = new ListNode(head.Value);
+                    currentPos = positiveList;
+                }
+                else
+                {
+                    currentPos.Next = new ListNode(head.Value);
+                    currentPos = currentPos.Next;
+                }
+            }
+            else
+            {
+                if (negativeList == null)
+                {
+                    negativeList = new ListNode(head.Value);
+                    currentNeg = negativeList;
+                }
+                else
+                {
+                    currentNeg.Next = new ListNode(head.Value);
+                    currentNeg = currentNeg.Next;
+                }
+            }
+
+            head = head.Next;
+        }
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ListNode head = new ListNode(1);
+        head.Next = new ListNode(-2);
+        head.Next.Next = new ListNode(3);
+        head.Next.Next.Next = new ListNode(-4);
+        head.Next.Next.Next.Next = new ListNode(5);
+
+        Console.WriteLine("Початковий список:");
+        PrintList(head);
+
+        ListNode positiveList;
+        ListNode negativeList;
+        Solution.SplitList(head, out positiveList, out negativeList);
+
+        Console.WriteLine("Позитивний список:");
+        PrintList(positiveList);
+
+        Console.WriteLine("Негативний список:");
+        PrintList(negativeList);
+
+        Console.ReadLine();
+    }
+
+    static void PrintList(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.Value + " ");
+            head = head.Next;
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[TestClass]
+public class SolutionTests
+{
+    [TestMethod]
+    public void TestSplitList_PositiveAndNegativeListsAreCorrect()
+    {
+        ListNode head = new ListNode(1);
+        head.Next = new ListNode(-2);
+        head.Next.Next = new ListNode(3);
+        head.Next.Next.Next = new ListNode(-4);
+        head.Next.Next.Next.Next = new ListNode(5);
+
+        ListNode positiveList;
+        ListNode negativeList;
+        Solution.SplitList(head, out positiveList, out negativeList);
+
+        Console.WriteLine("Positive list:");
+        PrintList(positiveList);
+        Console.WriteLine("Negative list:");
+        PrintList(negativeList);
+
+        string expectedPositive = "1 3 5";
+        string actualPositive = ConvertToString(positiveList);
+        Assert.AreEqual(expectedPositive, actualPositive, "Positive list is not correct");
+
+        string expectedNegative = "-2 -4";
+        string actualNegative = ConvertToString(negativeList);
+        Assert.AreEqual(expectedNegative, actualNegative, "Negative list is not correct");
+    }
+
+    private string ConvertToString(ListNode head)
+    {
+        string result = "";
+        while (head != null)
+        {
+            result += head.Value + " ";
+            head = head.Next;
+        }
+        return result.Trim();
+    }
+
+    private void PrintList(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.Value + " ");
+            head = head.Next;
+        }
+        Console.WriteLine();
+    }
+}
